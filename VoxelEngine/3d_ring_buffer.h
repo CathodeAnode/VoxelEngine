2def4168a460fb87d0adc49e95443316bc31d7d4
omar-owis <oomar@vt.edu>
2025-09-23 22:38:51 -0400

    Add circular indexing logic

--- VoxelEngine/3d_ring_buffer.h
+++ VoxelEngine/3d_ring_buffer.h
@@ -16,9 +16,12 @@ public:
 	inline Datatype_& At(const glm::ivec3& coords);
 	inline Datatype_ At(const glm::ivec3& coords) const;
 
+	inline size_t GetLength() { return m_Length; }
+
 private:
 	std::vector<Datatype_> m_RawBuffer;
 	size_t m_Length;
+
 };
 
 
@@ -31,20 +34,38 @@ inline RingBuffer3D<Datatype_>::RingBuffer3D(size_t size)
 template<typename Datatype_>
 inline void RingBuffer3D<Datatype_>::Set(const glm::ivec3 & coords, const Datatype_ & data)
 {
-	buffer[coords.x + coords.z * m_Length + coords.y * m_Length * m_Length] = data;
+	glm::ivec3 index(
+		coords.x % m_Length,
+		coords.y % m_Length,
+		coords.z % m_Length
+	);
+
+	buffer[index.x + index.z * m_Length + index.y * m_Length * m_Length] = data;
 }
 
 
 template<typename Datatype_>
 inline Datatype_& RingBuffer3D<Datatype_>::At(const glm::ivec3& coords)
 {
-	return buffer[coords.x + coords.z * m_Length + coords.y * m_Length * m_Length];
+	glm::ivec3 index(
+		coords.x % m_Length,
+		coords.y % m_Length,
+		coords.z % m_Length
+	);
+
+	return buffer[index.x + index.z * m_Length + index.y * m_Length * m_Length];
 }
 
 template<typename Datatype_>
 inline Datatype_ RingBuffer3D<Datatype_>::At(const glm::ivec3& coords) const
 {
-	return buffer[coords.x + coords.z * m_Length + coords.y * m_Length * m_Length];
+	glm::ivec3 index(
+		coords.x % m_Length,
+		coords.y % m_Length,
+		coords.z % m_Length
+	);
+
+	return buffer[index.x + index.z * m_Length + index.y * m_Length * m_Length];
 }
 
 #endif
