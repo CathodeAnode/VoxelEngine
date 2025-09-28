dea1de2712485c5124a321628d27647f00802496
omar-owis <oomar@vt.edu>
2025-09-27 23:02:24 -0400

    Add ChunkProvider C++20 concept

---
+++ VoxelEngine/IChunkContainer.h
@@ -0,0 +1,18 @@
+#ifndef INTERFACE_CHUNK_CONTAINER_H
+#define INTERFACE_CHUNK_CONTAINER_H
+
+#include <memory>
+#include <type_traits>
+
+// Forward declare glm::ivec3
+namespace glm {
+    struct ivec3;
+}
+
+template <typename T, typename ChunkType>
+concept ChunkProvider = requires(T t, const glm::ivec3 & chunkCoords) {
+    { t.GetChunk(chunkCoords) } -> std::same_as<std::shared_ptr<ChunkType>>;
+    { t.GetChunk(chunkCoords) } -> std::same_as<std::shared_ptr<const ChunkType>>;
+};
+
+#endif
